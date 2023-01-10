6bca39e59e744c798c8d8067602c08243e0d2297
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2023-01-11 00:49:09 +0600

    Template has been added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,36 +1,34 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+@include('admin.layouts.includes.header');
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<body>
+    <div class="loader"></div>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            
+            <!-- Navbar -->
+            @include('admin.layouts.includes.nav');
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('admin.layouts.navigation')
+            <!-- Sidebar -->
+            @include('admin.layouts.includes.sidebar');
+            
+            <!-- Main Content -->
+            @yield('mainpage');
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <!-- Footer -->
+            @include('admin.layouts.includes.footer');
         </div>
-    </body>
+    </div>
+
+
+    @include('admin.layouts.includes.scripts');
+   
+</body>
+
+
+<!-- index.html  21 Nov 2019 03:47:04 GMT -->
+
 </html>
